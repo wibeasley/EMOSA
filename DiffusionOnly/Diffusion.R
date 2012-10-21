e725f56ba61843073ebc749bd62a40f40a3c4499
Will Beasley <wibeasley@hotmail.com>
2012-10-21 13:36:41 -0500

    Renamed directory to 'DiffusionOnly'
    It used to be  'OneShot_Only1984Diffusion'

--- DiffusionOnly/Diffusion.R
+++ DiffusionOnly/Diffusion.R
@@ -4,7 +4,7 @@ require(rjags)
 
 
 if( Sys.info()["nodename"] == "MICKEY" ) 
-  pathDirectory <- "F:/Users/wibeasley/Documents/Consulting/EmosaMcmc/Dev/EmosaFork/EMOSA/OneShot_Only1984Diffusion"
+  pathDirectory <- "F:/Users/wibeasley/Documents/Consulting/EmosaMcmc/Dev/EmosaFork/EMOSA/DiffusionOnly"
   #pathDirectory <- "F:/Users/wibeasley/Documents/Consulting/EmosaMcmc/Dev/EMOSA/OneShot_Only1984Diffusion"
 if( Sys.info()["nodename"] == "MERKANEZ-PC" ) 
   pathDirectory <- "F:/Users/wibeasley/Documents/SSuccess/InterimStudy" #Change this directory location
