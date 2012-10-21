ac6376459c78c6fe6ff45c630a1eadee7867c130
Will Beasley <wibeasley@hotmail.com>
2012-10-21 13:47:41 -0500

    Diffusion.R reads from data file
    instead of hard-coded proportions

--- DiffusionOnly/Diffusion.R
+++ DiffusionOnly/Diffusion.R
@@ -4,29 +4,33 @@ require(rjags)
 
 
 if( Sys.info()["nodename"] == "MICKEY" ) 
-  pathDirectory <- "F:/Users/wibeasley/Documents/Consulting/EmosaMcmc/Dev/EmosaFork/EMOSA/DiffusionOnly"
+  pathDirectory <- "F:/Users/wibeasley/Documents/Consulting/EmosaMcmc/Dev/EMOSA"
   #pathDirectory <- "F:/Users/wibeasley/Documents/Consulting/EmosaMcmc/Dev/EMOSA/OneShot_Only1984Diffusion"
 if( Sys.info()["nodename"] == "MERKANEZ-PC" ) 
   pathDirectory <- "F:/Users/wibeasley/Documents/SSuccess/InterimStudy" #Change this directory location
 
-# pathModel <- file.path(pathDirectory, "DiffusionGauss.bugs")
-# pathModel <- file.path(pathDirectory, "DiffusionLogit.bugs")
-pathModel <- file.path(pathDirectory, "DiffusionBeta.bugs")
-# curve(dbeta(x, .1,.1))
+# pathModel <- file.path(pathDirectory, "DiffusionOnly/DiffusionGauss.bugs")
+# pathModel <- file.path(pathDirectory, "DiffusionOnly/DiffusionLogit.bugs")
+pathModel <- file.path(pathDirectory, "DiffusionOnly/DiffusionBeta.bugs")
+pathData <- file.path(pathDirectory, "Data/SummaryBirthYearByTime.csv")
 # curve(dbeta(x, 1,1))
 # curve(dbeta(x, 10,10))
 # curve(dlogis(x, location = .25, scale = 1), xlim=c(-5, 5))
-# curve(dlogis(x, location = 1.25, scale = 1), xlim=c(-5, 5), add=T)
-# curve(dlogis(x, location = .25, scale = 10), xlim=c(-5, 5))
-# 
-# curve(plogis(x, location = .25, scale = 1), xlim=c(-5, 5))
-# curve(qlogis(x, location = .25, scale = 1), xlim=c(0, 1))
+
+cohortYear <- 1984 #1980, 1981, 1982, 1983, 1984
+ds <- read.csv(pathData, stringsAsFactors=FALSE)
+ds <- ds[ds$byear == cohortYear, ] #Select only the desired cohort
+ds <- ds[order(ds$time), ] #Sort, just, to make sure values will be passed to JAGS in the correct order.
+
+pg <- ds$ProportionGoers
+pi <- ds$ProportionIrregulars
+pa <- ds$ProportionAbsentees
 
 #Proportion of Goers, of Irregulars, or Nongoers (or absentees) {Check these with data; I may have messed up the order}
 #For the 1984 cohort
-pg <- c(0.401088929, 0.340290381,	0.249546279,	0.218693285,	0.180580762,	0.167876588,	0.157894737,	0.158802178,  0.161524501)
-pi <- c(0.233212341, 0.256805808,	0.288566243,	0.305807623,	0.27676951,	  0.270417423,	0.229582577,	0.250453721,  0.237749546)
-pa <- c(0.36569873,  0.402903811,	0.461887477,	0.475499093,	0.542649728,	0.561705989,	0.612522686,	0.590744102,  0.600725953)
+# pg <- c(0.401088929, 0.340290381,	0.249546279,	0.218693285,	0.180580762,	0.167876588,	0.157894737,	0.158802178,  0.161524501)
+# pi <- c(0.233212341, 0.256805808,	0.288566243,	0.305807623,	0.27676951,	  0.270417423,	0.229582577,	0.250453721,  0.237749546)
+# pa <- c(0.36569873,  0.402903811,	0.461887477,	0.475499093,	0.542649728,	0.561705989,	0.612522686,	0.590744102,  0.600725953)
 timeCount <- length(pg)
 if( length(pi) != timeCount) stop("The proportions have a different number of time points.")
 if( length(pa) != timeCount) stop("The proportions have a different number of time points.")
@@ -54,7 +58,7 @@ chains <- coda.samples(jagsModel, variable.names=parametersToTrack, n.iter=count
 elapsed  <- Sys.time() - startTime
 (condensed <- summary(chains))
 
-# windows()
+# windows() # dev.off()
 gelman.diag(chains, autoburnin=FALSE) #This is R-hat; the burnin period is manually specified above, so turn off the auto argument. 
 effectiveSize(chains) #Sample size adjusted for autocorrelation
 
